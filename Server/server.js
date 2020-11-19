2269d6af78ecbb4f08c31c8d0ad0aa4dbb00a456
beatriz.martinez.perez <190343@utags.edu.mx>
2020-11-19 10:25:33 -0600

    Primer commit del proyecto

---
+++ Server/server.js
@@ -0,0 +1,69 @@
+require('./config/config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    res.send('<h1>Bienvenido a mi servidor REST</h1>');
+});
+
+app.get('/usuario', function(req, res){
+    res.json({
+        ok: 200,
+        mensaje: 'Usuarios consultados con éxito'
+    });
+});
+
+app.get('/saludo', function(req, res){
+    res.json({
+        ok: '200',
+        mensaje: 'Bienvenido :)'
+    });
+});
+
+app.post('/usuario', function(req, res){
+    let nombre = req.body.nombre;
+    let body = req.body;
+    
+    if(nombre == undefined){
+        res.status(400).json({
+            ok: 400,
+            mensaje: 'Favor de enviar el valor del nombre' 
+        });
+    }else{
+        res.json({
+            ok: 200,
+            mensaje: 'Usuario insertado con éxito',
+            nombre: nombre,
+            body: body
+    });
+    }
+});
+
+app.put('/usuario/:id/:nombre', function(req, res){
+    let id = req.params.id;
+    let nombre = req.params.nombre;
+
+    res.json({
+        ok: 200,
+        mensje: 'Usuario actualizado con éxito',
+        id: id,
+        nombre: nombre
+    })
+});
+
+app.delete('/usuario/:id', function(req, res){
+    let id = req.params.id;
+    res.json({
+        ok: 200,
+        mensaje: 'Usuario eliminado con éxito',
+        id: id
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('El servidor está en línea en el puerto ', process.env.PORT);
+});
